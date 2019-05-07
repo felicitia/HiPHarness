b63aae743bad9b903c34b7a0cc7f151e3053554a
heymind <heymind@outlook.com>
2019-05-07 14:37:18 +0800

    sliding window model tester

--- sliding_window_model_tester.py
+++ sliding_window_model_tester.py
@@ -1,10 +1,10 @@
 import time
-
+import math
 
 class SlidingWindowModelTester:
     urls = []
 
-    def __init__(self, window_size, model_build_func, history_count=10):
+    def __init__(self, window_size, model_build_func, history_count=1):
         """
         Sliding Window Prefetching
         :param window_size:   (“URL size” = 50, 100, 200, 300, 400, 500)
@@ -26,7 +26,6 @@ class SlidingWindowModelTester:
 
     def train_and_test(self, train_ratio=0.8):
         total_urls = len(self.urls)
-
         cache_set = set()
         hit_set = set()
         miss_set = set()
@@ -34,25 +33,30 @@ class SlidingWindowModelTester:
         miss_count = 0
         prefetch_count = 0
 
-        # train model with first (window_size * train_ratio) urls
-        model = self.model_build_func()
-        for url in self.urls[0:int(self.window_size * train_ratio)]:
-            model.feed(url)
+        begin = math.ceil(self.window_size * train_ratio)
 
-        begin = int(self.window_size * train_ratio)
-        end = self.window_size
+        end = self.window_size 
         t0 = time.time()
-
+       
         while end < total_urls:
+            # print(begin,end,begin-math.ceil(self.window_size * train_ratio))
+            model = self.model_build_func()
+            for url in self.urls[begin-math.ceil(self.window_size * train_ratio):begin]:
+                model.feed(url)
+
             while begin < end:
                 # predict engine
+                # continuous training
+                current_url = self.urls[begin]
+                model.feed(current_url)
+                # print(self.urls[begin - self.history_count:begin][0] ==self.urls[begin - 1] )
                 predict_urls = model.predict(self.urls[begin - self.history_count:begin])
                 for predict_url in predict_urls:
                     if predict_url not in cache_set:
                         cache_set.add(predict_url)
                         prefetch_count += 1
 
-                current_url = self.urls[begin]
+
                 if current_url in cache_set:
                     hit_set.add(current_url)
                     hit_count += 1
@@ -61,23 +65,29 @@ class SlidingWindowModelTester:
                     miss_set.add(current_url)
                     miss_count += 1
 
-                # continuous training
-                model.feed(current_url)
+
 
                 begin += 1
 
-            precision = len(hit_set) / (len(hit_set) or 1)
-            recall = (hit_count / miss_count + hit_count) if (miss_count + hit_count) else 0
-
+            precision = len(hit_set) / (len(cache_set) or 1)
+            recall = (hit_count / (miss_count + hit_count)) if (miss_count + hit_count) else 0
+            # print("p",precision,len(hit_set),len(cache_set) )
+            # print("r",recall,hit_count+miss_count)
             # yield stage result
+
             yield len(cache_set), len(hit_set), len(
                 miss_set), hit_count, miss_count, prefetch_count, precision, recall, time.time() - t0
 
             # sliding
-            end += int(self.window_size * train_ratio)
+    
+            end += math.ceil(self.window_size * (1-train_ratio))
 
-            # should I clear all set here ?
-            # cache_set = set()
-            # hi_set = set() ....
+            # should I clear all set here ? -- Yes
+            cache_set.clear()
+            hit_set.clear()
+            miss_set.clear()
+            hit_count = 0
+            miss_count = 0
+            prefetch_count = 0
 
         return False
